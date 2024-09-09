3d5f9e77f62378889e57c292791fb402b3be29e0
Ali Syed <alisyed0206@gmail.com>
2024-09-09 11:13:55 -0400

    finished part 1 of this proejct and uploading to the repo

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ali Syed on 2024-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ali Syed on 2024-09-06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
